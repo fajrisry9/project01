b5040734eec847b08f33688a9008c55f910a3900
fajrisry09 <fajrisry.09@gmail.com>
2022-09-03 14:39:41 +0700

    Menambahkan beberapa kode

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ex5d4.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -20,11 +21,27 @@ def save_diary():
     title_receive = request.form["title_give"]
     content_receive = request.form["content_give"]
 
+    today = datetime.now()
+    mytime = today.strftime('%d-%m-%Y %H-%M-%S')
+
+    file = request.files["file_give"]
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    profile = request.files["profile_give"]
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
+
     doc = {
+        'file':filename,
+        'profile':profilename,
         'title':title_receive,
         'content':content_receive
     }
     db.diary.insert_one(doc)
+    
 
     return jsonify({'msg':'Upload selesai'})
 
