0c863792cf0e7fb3d9764d72957becb7fdc4bc00
fajrisry09 <fajrisry.09@gmail.com>
2022-09-03 10:40:33 +0700

    mengubah app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def save_diary():
     }
     db.diary.insert_one(doc)
 
-    return jsonify({'msg':'Upload selesai!'})
+    return jsonify({'msg':'Upload selesai'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
